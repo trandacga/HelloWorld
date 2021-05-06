6a5d925bd2d8548593ca0227a95a2e6ee7d587d8
trandacga <gatran1402@gmail.com>
2021-05-06 16:25:51 +0700

    first commit

--- src/basic/dev/MainApp.java
+++ src/basic/dev/MainApp.java
@@ -3,8 +3,8 @@ package basic.dev;
 public class MainApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// TODO Auto-generated method stub	
+		System.out.println("Hello World");
 	}
 
 }
